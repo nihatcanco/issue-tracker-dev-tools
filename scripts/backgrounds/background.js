468f1e399ac80c1166a6f30aa58a3c4d8b63e9df
nihatcanco <nihaatcan.colpan@gmail.com>
2018-11-21 12:04:14 +0300

    Fix on default commit message format setting.

--- scripts/backgrounds/background.js
+++ scripts/backgrounds/background.js
@@ -1,3 +1,16 @@
-(function (global) {
+var commitMessageFormatDefault = '{TICKET_TYPE}({TICKET_NUMBER}): {TICKET_SUMMARY}{NEWLINE}{NEWLINE}';
 
-}(window));
+chrome.runtime.onInstalled.addListener(function (details) {
+
+    console.log(JSON.stringify(details));
+
+    // Set the defaults for initial launch
+    chrome.storage.sync.get(['commitMessageFormat'], function (result) {
+
+        if (!result || !result.commitMessageFormat) {
+            chrome.storage.sync.set({ commitMessageFormat: commitMessageFormatDefault }, function () { });
+        }
+
+    });
+
+});
