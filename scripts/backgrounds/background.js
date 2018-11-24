561415d6e33c44364c72552c7ae9472e4d51b006
nihatcan <nihatcan.colpan@gmail.com>
2018-11-24 21:10:30 +0300

    Removed jQuery UI lib.

--- scripts/backgrounds/background.js
+++ scripts/backgrounds/background.js
@@ -1,44 +1,48 @@
-var commitMessageFormatDefault = '{LOWERCASE}{TICKET_TYPE}{/LOWERCASE}({UPPERCASE}{TICKET_NUMBER}{/UPPERCASE}): {TICKET_SUMMARY}{NEWLINE}{NEWLINE}';
+(function (global) {
 
-chrome.runtime.onInstalled.addListener(function (details) {
+    var commitMessageFormatDefault = '{LOWERCASE}{TICKET_TYPE}{/LOWERCASE}({UPPERCASE}{TICKET_NUMBER}{/UPPERCASE}): {TICKET_SUMMARY}{NEWLINE}{NEWLINE}';
 
-    console.log(JSON.stringify(details));
+    chrome.runtime.onInstalled.addListener(function (details) {
 
-    // Set the defaults for initial launch
-    chrome.storage.sync.get(['commitMessageFormat'], function (result) {
+        console.log(JSON.stringify(details));
 
-        if (!result || !result.commitMessageFormat) {
-            chrome.storage.sync.set({ commitMessageFormat: commitMessageFormatDefault }, function () { });
-        }
+        // Set the defaults for initial launch
+        chrome.storage.sync.get(['commitMessageFormat'], function (result) {
 
-    });
-
-});
-
-function openOrFocusOptionsPage() {
-
-    var optionsUrl = chrome.extension.getURL('options.html');
-
-    chrome.tabs.query({}, function (extensionTabs) {
-
-        let found = false;
-
-        for (let i = 0; i < extensionTabs.length; i++) {
-            if (optionsUrl == extensionTabs[i].url) {
-                found = true;
-                chrome.tabs.update(extensionTabs[i].id, { "selected": true });
+            if (!result || !result.commitMessageFormat) {
+                chrome.storage.sync.set({ commitMessageFormat: commitMessageFormatDefault }, function () { });
             }
-        }
 
-        if (!found) {
-            chrome.tabs.create({ url: "options.html" });
-        }
+        });
 
     });
 
-}
+    function openOrFocusOptionsPage() {
 
-// Called when the user clicks on the browser action icon.
-chrome.browserAction.onClicked.addListener(function (tab) {
-    openOrFocusOptionsPage();
-});
+        var optionsUrl = chrome.extension.getURL('options.html');
+
+        chrome.tabs.query({}, function (extensionTabs) {
+
+            let found = false;
+
+            for (let i = 0; i < extensionTabs.length; i++) {
+                if (optionsUrl == extensionTabs[i].url) {
+                    found = true;
+                    chrome.tabs.update(extensionTabs[i].id, { "selected": true });
+                }
+            }
+
+            if (!found) {
+                chrome.tabs.create({ url: "options.html" });
+            }
+
+        });
+
+    }
+
+    // Called when the user clicks on the browser action icon.
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        openOrFocusOptionsPage();
+    });
+
+}(window));
